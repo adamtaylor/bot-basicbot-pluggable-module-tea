90ef1122bf48bf716dafd0b30cfc957d17368e80
Adam Taylor <adam.taylor@net-a-porter.com>
2011-10-27 17:01:59 +0100

    Add in support for random tea making

--- lib/Bot/BasicBot/Pluggable/Module/Tea.pm
+++ lib/Bot/BasicBot/Pluggable/Module/Tea.pm
@@ -25,8 +25,9 @@ Then when you fancy a brew, just issue the C<!tea> command:
 
 sub help {
     my $help = "This plugin helps facilitae tea making within a team. Simply "
-        . "issue the `!tea` command when you fancy a brew and someone will "
-        . "be selected at random from the channel to make the tea round";
+        . "issue the `!tea` command when you fancy a brew and a new tea round"
+        . "will begin. If you want to live on the edge issue the `!russiantea`"
+        . "command to have someone at random selected to make the tea";
 
     return $help;
 }
@@ -41,9 +42,9 @@ sub help {
         my $who  = $msg->{who};
         my $chan = $msg->{channel};
 
-        if ( $body =~ /!tea/ ) {
+        my @all_nicks = $self->bot->pocoirc->channel_list( $chan );
 
-            my @all_nicks = $self->bot->pocoirc->channel_list( $chan );
+        if ( $body =~ /^!tea$/ ) {
 
             for my $nick (@all_nicks) {
                 # insert new nick if not already in list and isn't the bot itself
@@ -63,6 +64,17 @@ sub help {
 
             # take the first nick and put them to the back of the list
             push @nick_list, shift @nick_list;
+
+            return $resp;
+        }
+
+        if ( $body =~ /^!russiantea$/ ) {
+            # Choose a random nick from the channel
+            do {
+                $brew_maker = $all_nicks[int(rand(scalar @all_nicks - 1))];
+            } until $brew_maker ne $self->bot->nick;
+
+            my $resp = "$who would like a brew! $brew_maker: your turn!";
 
             return $resp;
         }
