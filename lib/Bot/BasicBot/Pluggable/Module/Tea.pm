7a9cbd7b4e5813c9d394cfa2413bb2d74ddfb28a
Kristian Flint <kgflint@gmail.com>
2011-12-18 13:36:00 +0000

    Take crack away and make nick list always available and correct
    The @selected_nick_list has been taken out as it's use with the actual nick_list wasn't really working. Now it works on the assumption that the end of the array was the last person to make tea, which is probably safe enough.
    Also the nick list exists even before someone calls the first tea although I've just realised that if the cache has expried you'll still get reshuffled.

--- lib/Bot/BasicBot/Pluggable/Module/Tea.pm
+++ lib/Bot/BasicBot/Pluggable/Module/Tea.pm
@@ -25,7 +25,6 @@ Then when you fancy a brew, just issue the C<!tea> command:
 
 =cut
 
-
 sub help {
     my $help = "This plugin helps facilitae tea making within a team. Simply "
         . "issue the `!tea` command when you fancy a brew and a new tea round"
@@ -37,14 +36,32 @@ sub help {
 
 {
     my @nick_list;
-    my @selected_nick_list;
     my $last_used = DateTime->now;
 
     sub select_brew_maker {
         my $self = shift;
         my $chan = shift;
+        
+        my $extra = '';
+        if (DateTime->now > $last_used->clone->add(hours => 8)) {
+            @nick_list = shuffle(@nick_list);
+            $extra = ' (the rota was rewritten due to inactivity)';
+        }
 
-        my @all_nicks = $self->bot->pocoirc->channel_list( $chan );
+        my $brew_maker = $nick_list[0];
+
+        # take the first nick and put them to the back of the list
+        push @nick_list, shift @nick_list;
+
+        $last_used = DateTime->now;
+
+        return ($brew_maker,$extra);
+    }
+
+    sub chanjoin {
+        my ( $self, $msg ) = @_;
+		# someone joined look at the current array of nicks and add the new person in a random place
+		my @all_nicks = $self->bot->pocoirc->channel_list( $msg->{channel} );
 
         for my $nick (@all_nicks) {
             # insert new nick if not already in list and isn't the bot itself
@@ -52,31 +69,23 @@ sub help {
                 splice(@nick_list, int(rand(@nick_list - 1)), 0, $nick);
             }
         }
+	}
+	
+	#didn't work so instead we tidy_lists as part of the start of told
+    # sub chanpart {
+    #         my ( $self, $msg ) = @_;
+    #       # someone left the room rebuild the lists but don't change the order
+    #         $self->tidy_lists( $msg->{channel} );
+    #   }
 
-        my $extra = '';
-        if (DateTime->now > $last_used->clone->add(hours => 8)) {
-            @nick_list = shuffle(@nick_list);
-            @selected_nick_list = [];
-            $extra = ' (the rota was rewritten due to inactivity)';
-        }
-
-        # rotate list until first nick is in the room
-        while (!grep {$nick_list[0] eq $_} @all_nicks) {
-            push @nick_list, shift @nick_list;
-        }
-
-        my $brew_maker = $nick_list[0];
-
-        # take the first nick and put them to the back of the list
-        push @nick_list, shift @nick_list;
-        # maintain a list of previously selected tea makers
-        push @selected_nick_list, $brew_maker;
-
-        $last_used = DateTime->now;
-
-        return ($brew_maker,$extra);
+    sub tidy_lists {
+        my ( $self, $chan ) = @_;
+        my @all_nicks = $self->bot->pocoirc->channel_list( $chan );
+        # don't tell Gianni I used ~~
+        @nick_list = grep { $_ ~~ @all_nicks && $_ ne $self->bot->nick } @nick_list;
+        # @selected_nick_list = grep { $_ ~~ @all_nicks } @selected_nick_list;
     }
-
+    
     sub told {
         my ( $self, $msg ) = @_;
 
@@ -85,7 +94,10 @@ sub help {
         my $chan = $msg->{channel};
 
         my @all_nicks = $self->bot->pocoirc->channel_list( $chan );
-
+        #init the nick list if we don't have it already
+        $self->chanjoin( $msg ) unless $#nick_list > 0;
+        $self->tidy_lists( $msg->{channel} );
+        
         if ( $body =~ /^!tea$/ ) {
 
             my ($brew_maker,$extra) = $self->select_brew_maker( $chan );
@@ -101,10 +113,10 @@ sub help {
 
             if ( $commands[1] eq 'away' ) {
                 # Choose a new tea maker and pop the previous maker back
-                my $previous_tea_maker = pop @selected_nick_list;
+                my $person_away = pop @nick_list;
                 my ($brew_maker, $extra) = $self->select_brew_maker( $chan );
-                unshift @nick_list, $previous_tea_maker;
-                return "$who says $previous_tea_maker is AWOL. $brew_maker, take over!";
+                unshift @nick_list, $person_away;
+                return "$who says $person_away is AWOL. $brew_maker, take over!";
             }
             elsif ( $commands[1] eq 'volunteer' ) {
                 # If you volunteer, go to back of list
